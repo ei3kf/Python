c8fc687cafe9f1cd3ff888646f67e9d35cdfed88
Depech Vogel <depechvogelnl@gmail.com>
2015-07-23 12:06:24 +0000

    230715:1206

--- my_ip.py
+++ my_ip.py
@@ -16,67 +16,88 @@ import argparse
 
 
 def my_ip():
-    http_response = get_www()
+    """
+    Returns the IP address of the client where the script is run.
+    """
+    http_response = get_www(url)
     my_ip = get_ip(http_response)
     return my_ip
 
 
-def get_www():
-    url = "http://checkip.dyndns.org:8245/"
+def get_www(url):
+    """
+    Connect to a URL, and return the response.
+    """
     try:
         http_response = urllib.urlopen(url).read()
         return http_response
     except Exception, e:
-        print "Error: Can't reach", url
+        print("Error: Can't reach {}").format(url)
         sys.exit(1)
 
 
 def check_my_ip(my_region, my_ip_address, my_security_group):
+    """
+    Checks to see if the given IP exists in the given AWS security group.
+    """
     ec2 = boto.ec2.connect_to_region(my_region)
     security_group = ec2.get_all_security_groups(group_ids=my_security_group)
     for sg in security_group:
         for rule in sg.rules:
             for ip in rule.grants:
-	        if str(ip) == my_ip_address:
-		    print my_ip_address + " found in " + my_security_group
-		    return
-    print my_ip_address + " NOT found in " + my_security_group
-    return
+                if str(ip) == my_ip_address:
+		    print("My IP {} found in Security Group {}").format(my_ip_address,my_security_group)
+                    return True
+    print("My IP {} NOT found in Security Group {}").format(my_ip_address,my_security_group)
+    return False
 
 
 def display_my_security_group(my_region,my_security_group):
+    """
+    List all the IP addresses in the given AWS security group.
+    """
     ec2 = boto.ec2.connect_to_region(my_region)
     security_group = ec2.get_all_security_groups(group_ids=my_security_group)
     for sg in security_group:
         for rule in sg.rules:
             for ip in rule.grants:
-	        print ip
+                print ip
     return
 
 
 def add_my_ip(my_region, my_ip_address, my_security_group):
+    """
+    Add the given IP to the given AWS security group, allowing all protocols and all network ports.
+    """
     ec2 = boto.ec2.connect_to_region(my_region)
     rule = ec2.authorize_security_group(
-	group_id=my_security_group,
-	ip_protocol="-1", 
-	from_port=-1,
-	to_port=-1,
-	cidr_ip=my_ip_address) 
+        group_id=my_security_group,
+        ip_protocol="-1", 
+        from_port=-1,
+        to_port=-1,
+        cidr_ip=my_ip_address) 
     return
   
 
 def delete_my_ip(my_region, my_ip_address, my_security_group):
+    """
+    Delete the given IP from the given AWS security group.
+    """
     ec2 = boto.ec2.connect_to_region(my_region)
     rule = ec2.revoke_security_group(
-	group_id=my_security_group,
-	ip_protocol="-1",
-	from_port=-1,
-	to_port=-1,
-	cidr_ip=my_ip_address) 
+        group_id=my_security_group,
+        ip_protocol="-1",
+        from_port=-1,
+        to_port=-1,
+        cidr_ip=my_ip_address) 
     return
 
 
 def get_ip(http_response):
+    """
+    Find the IP address in the url response.
+    Return that IP   
+    """
     response_ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", http_response)
     my_ip = ''.join(response_ip) + "/32"
     return my_ip
@@ -89,7 +110,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--region",
         help="AWS Region",
-	default="eu-west-1",
+        default="eu-west-1",
+        type=str)
+
+    parser.add_argument(
+        "--url",
+        help="URL to retrieve current IP address",
+        default="http://checkip.dyndns.org:8245/",
         type=str)
 
     parser.add_argument(
@@ -116,13 +143,15 @@ if __name__ == "__main__":
         action='store_true',
         default=False)
 
-    parser.add_argument(
+    add_delete = parser.add_mutually_exclusive_group()
+
+    add_delete.add_argument(
        "--add",
         help="Add my IP address to my Security Group.",
         action='store_true',
         default=False)
 
-    parser.add_argument(
+    add_delete.add_argument(
        "--delete",
         help="Delete my IP address from my Security Group.",
         action='store_true',
@@ -130,28 +159,40 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    url = args.url
+
     my_ip_address = my_ip()
     my_security_group = args.sg
     my_region = args.region
    
     try:
         if args.check:
-            print "Checking for " + my_ip_address + " in " + my_security_group
+            print("Checking for {} in {}").format(my_ip_address, my_security_group)
             check_my_ip(my_region, my_ip_address, my_security_group)
-	elif args.show:
-	    print "Displaying IPs in " + my_security_group
-	    display_my_security_group(my_region,my_security_group)
+        elif args.show:
+            print("Displaying IPs in {}").format(my_security_group)
+            display_my_security_group(my_region,my_security_group)
         elif args.ip:
-            print "Your IP address is " + my_ip_address
+            print("Your IP address is {}").format(my_ip_address)
         elif args.add:
-	    print "Adding " + my_ip_address + " to " + my_security_group
-            add_my_ip(my_region, my_ip_address, my_security_group)
+	    checked_my_ip = check_my_ip(my_region, my_ip_address, my_security_group)
+	    if checked_my_ip:
+                print("{} already exists in {}, nothing further to do.").format(my_ip_address, my_security_group)
+	    else:
+                print("Adding {} to {}").format(my_ip_address, my_security_group)
+                add_my_ip(my_region, my_ip_address, my_security_group)
         elif args.delete:
-	    print "Deleting " + my_ip_address + " from " + my_security_group
-            delete_my_ip(my_region, my_ip_address, my_security_group)
+	    checked_my_ip = check_my_ip(my_region, my_ip_address, my_security_group)
+	    if checked_my_ip:
+                print("Deleting {} from {}").format(my_ip_address, my_security_group)
+                delete_my_ip(my_region, my_ip_address, my_security_group)
+	    else:
+                print("{} NOT found in {}, nothing further to do.").format(my_ip_address, my_security_group)
+
         else:
             print "Niks te doen."
     except KeyboardInterrupt:
-	sys.exit(0)
+        sys.exit(0)
     except Exception, e:
         print "Computer says " + e
+
