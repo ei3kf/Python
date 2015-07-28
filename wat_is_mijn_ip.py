1e25e08db05b47ab4f82b56611e112d1a8bb51ac
Depech Vogel <depechvogelnl@gmail.com>
2015-07-28 14:42:07 +0000

    280715:1442

--- wat_is_mijn_ip.py
+++ wat_is_mijn_ip.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 """
+
 Find and return your IP address.
-- refactored to use a class object. 
+- refactored to use a class object for mentorship.
+
 """
 
 import urllib
@@ -16,26 +18,30 @@ class MijnIp(object):
 
     def __init__(self, url):
         self.url = url
-	self.response = ""
+	self.http_response = None
+	self.response_ip = None
+
 
     def read_url(self):
-	"""	
-	  read_url() - this function will access a URL and return the response.
-	"""	
-        http_response = urllib.urlopen(self.url).read()
-	return http_response
+        """     
+          read_url() - this function will access a URL and return the response.
+        """     
+        self.http_response = urllib.urlopen(self.url).read()
 
-    def return_ip(self, http_response):
-	"""
-	  return_ip() - this function will return the IP address found in the read_url() returned response.
-	"""
-        response_ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", http_response) 
-	return response_ip
+
+    def return_ip(self):
+        """
+          return_ip() - assign the IP address found to an variable.
+        """
+        self.response_ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", self.http_response)
+
+
+
 
 url = "http://checkip.dyndns.org:8245/"
 
 my_ip_query = MijnIp(url)
-my_ip = my_ip_query.return_ip(my_ip_query.read_url())
-print my_ip
+my_ip_query.read_url()
+my_ip_query.return_ip()
 
-
+print my_ip_query.response_ip
