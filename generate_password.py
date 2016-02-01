8cf56da63d18b753bb6e6256c7dd2ce9b150222c
andy_clements <eithreekf@gmail.com>
2016-02-01 20:45:34 +0000

    First commit of generate_password.py

---
+++ generate_password.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+"""
+generate_password.py
+  - create alphanumeric including special characters password.
+"""
+
+import random
+import string
+import argparse
+
+
+def generate_password(length):
+    """
+    generate a random alphanumeric with special
+    characters string of a given length.
+    """
+    password = ''.join([random.SystemRandom().choice(
+                       string.ascii_letters
+                       + string.digits
+                       + '!@#$%^&(')
+                       for n in range(length)])
+    return password
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--length",
+        help="Length of password to create.",
+        type=int,
+        default=16
+        )
+
+    args = parser.parse_args()
+    password = generate_password(args.length)
+    print("{}").format(password)
