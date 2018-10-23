a422ee79800de5549154e8159353844ea28f845e
Depech Vogel <eithreekf@gmail.com>
2018-10-23 09:51:16 +0100

    Create validateInstanceTypeValue.py

---
+++ validateInstanceTypeValue.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import boto3
+import time
+
+client = boto3.client('ec2', region_name='eu-west-1')
+
+i = 'put instance ID here'
+modifiedInstanceTypeValue = 't2.small'
+
+try:
+    instanceType = client.describe_instance_attribute(InstanceId=i,Attribute='instanceType')['InstanceType']
+    currentInstanceTypeValue = instanceType.get('Value')
+    while (currentInstanceTypeValue != modifiedInstanceTypeValue):
+        instanceType = client.describe_instance_attribute(InstanceId=i,Attribute='instanceType')['InstanceType']
+        currentInstanceTypeValue = instanceType.get('Value')
+        print(currentInstanceTypeValue)
+        print(modifiedInstanceTypeValue)
+        time.sleep(5)
+except Exception as e:
+    msg = describe_instance_attribute() has encountered an exception %s , error --> %s ' % (i, str(e))
+    print(msg)
+        
