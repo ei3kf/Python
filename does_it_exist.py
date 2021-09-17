dd5cabadf9b117ee79e399be0945ae99df6ab70f
Depech Vogel <3405575+ei3kf@users.noreply.github.com>
2021-09-17 08:23:51 +0100

    Update does_it_exist.py

--- does_it_exist.py
+++ does_it_exist.py
@@ -1,7 +1,12 @@
 ## Does whatever is in file one exist in file two?
 ## If it does, then its a True, if not it's a False.
 
-with open('input.sorted.txt') as f_a, open('output.sorted.txt') as f_b:
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+
+with open(a) as f_a, open(b) as f_b:
     a_lines = set(f_a.read().splitlines())
     b_lines = set(f_b.read().splitlines())
 for line in a_lines:
